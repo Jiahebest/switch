1181f15766e1fcd85b11e90449824f75792b023e
gdcp <gdcp@m25.local>
2018-03-13 11:16:42 +0800

    创建了UISwitch控件实例并为属性赋值 创建了UIPageControl控件并为属性赋值

--- switchTest/ViewController.swift
+++ switchTest/ViewController.swift
@@ -12,9 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGray
+        //创建控件实例并为属性赋值
+        let swi = UISwitch(frame: CGRect(x: 100, y: 100, width: 30, height: 30))
+        swi.onTintColor = UIColor.green
+        swi.tintColor = UIColor.gray
+        swi.thumbTintColor = UIColor.white
+        swi.addTarget(self, action: #selector(switchDidChange(swi:)), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(swi)
+        
+        
+        let page = UIPageControl(frame: CGRect(x: 150, y: 150, width: 100, height: 30))
+        page.numberOfPages = 4
+        page.currentPageIndicatorTintColor = UIColor.white
+        page.addTarget(self, action: #selector(changeValue(page:)), for: .valueChanged)
+        self.view.addSubview(page)
     }
-
+    func switchDidChange(swi:UISwitch){
+        if swi.isOn {
+            swi.onTintColor = UIColor.green
+            self.view.backgroundColor = UIColor.white
+        }
+        else{
+            self.view.backgroundColor = UIColor.darkGray
+        }
+    }
+    func changeValue(page:UIPageControl){
+        print(page.currentPage)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
